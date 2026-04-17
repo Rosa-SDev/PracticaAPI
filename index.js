9db49f8df441e6b2e234cc2018d70b61968ba453
Rosa <ripy36@gmail.com>
2026-04-17 09:39:25 -0500

    Primer endpoint

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-const express = require('express');
+const express = require('express')
+process.loadEnvFile()
+
+const app = express()
+
+const PORT = process.env.PORT
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(3000, () => {
+    console.log('Corriendo')
+})
+
